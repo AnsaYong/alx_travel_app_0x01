de76e8ab895244810c9332db30512f4b1ecf30d8
Ansahmbom <ansahmbomyong@gmail.com>
2024-12-25 01:43:21 +0200

    updated listings/serializers.py

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -3,6 +3,12 @@ from .models import Listing, Booking
 from django.contrib.auth.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
