a4fdbbe087c1a94c6136e8c3d3a029892cebcfab
Ansahmbom <ansahmbomyong@gmail.com>
2025-01-15 11:46:45 +0200

    added listings/views.py

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -47,4 +47,6 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("", include("listings.urls")),
+    path("", include("bookings.urls")),
 ]
